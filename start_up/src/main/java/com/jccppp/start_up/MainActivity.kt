d04575d4a5e77aec2f42ea57f51e6dee7c80a955
97569642@qq.com <QAZwsx56822>
2022-06-06 22:03:50 +0800

    Initial commit

---
+++ start_up/src/main/java/com/jccppp/start_up/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.jccppp.start_up
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
